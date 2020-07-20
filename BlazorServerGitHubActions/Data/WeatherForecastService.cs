937a39e4cc26088f1f412d6bd688a95e42ee16ca
Pivendren Naik <pivendren@gmail.com>
2020-07-20 18:27:04 +0200

    Appsettings

--- BlazorServerGitHubActions/Data/WeatherForecastService.cs
+++ BlazorServerGitHubActions/Data/WeatherForecastService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,13 @@ namespace BlazorServerGitHubActions.Data
 {
     public class WeatherForecastService
     {
+        private readonly IConfiguration _configuration;
+
+        public WeatherForecastService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -13,12 +21,16 @@ namespace BlazorServerGitHubActions.Data
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
+            var appSetting = _configuration.GetValue<string>("AppSettingValue");
+            var gitHub = _configuration.GetValue<string>("GitHubSecretValue");
+
             var rng = new Random();
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                //Summary = Summaries[rng.Next(Summaries.Length)
+                Summary = $"[{appSetting}][{gitHub}]"
             }).ToArray());
         }
     }
